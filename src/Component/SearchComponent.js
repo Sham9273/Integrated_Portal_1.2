9da8217c9594810bfc14700ac862deb494508b6a
vijayCdac <prachibhudeo26@gmail.com>
2025-08-04 11:47:53 +0530

    change in search bar

--- src/Component/SearchComponent.js
+++ src/Component/SearchComponent.js
@@ -41,34 +41,6 @@ const SearchComponent = () => {
 
             {/* Center - Search (Show only if not mobile and toggled on) */}
             {!isMobile && showSearch && (
-                // <motion.div
-                //   whileHover={{ scale: 1.03 }}
-                //   style={{
-                //     display: 'flex',
-                //     alignItems: 'center',
-                //     backgroundColor: 'rgba(240, 240, 240, 0.9)',
-                //     padding: '4px 16px',
-                //     borderRadius: 20,
-                //     boxShadow: 'inset 0 0 4px rgba(0,0,0,0.1)',
-                //     flex: 1,
-                //     maxWidth: 400,
-                //     marginLeft: 20,
-                //     marginRight: 20,
-                //   }}
-                // >
-                //   <SearchIcon sx={{ color: 'text.secondary', mr: 1, fontSize: 18 }} />
-
-                //   <InputBase
-                //     placeholder="Search…"
-                //     inputProps={{ 'aria-label': 'search' }}
-                //     sx={{
-                //       flex: 1,
-                //       fontSize: '0.85rem',
-                //       color: 'text.primary',
-                //       '&:focus': { outline: 'none' },
-                //     }}
-                //   />
-                // </motion.div>
 
                 <motion.div
                     whileHover={{ scale: 1.03 }}
@@ -76,8 +48,8 @@ const SearchComponent = () => {
                         display: 'flex',
                         alignItems: 'center',
                         backgroundColor: 'rgba(240, 240, 240, 0.9)',
-                        padding: '4px 16px',
-                        borderRadius: 20,
+                        padding: '2px 12px',
+                        borderRadius: 16,
                         boxShadow: 'inset 0 0 4px rgba(0,0,0,0.1)',
                         flex: 1,
                         maxWidth: 400,
