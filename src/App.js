ec05d2ab43f59990d81bf82b9fade7d293411830
Sham9273 <shamlomate2114@gmail.com>
2025-08-05 17:15:28 +0530

    Change banner background as a image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import Navbar from './Component/Navbar';
 import Home from './Pages/Home';
 import Footer from './Component/Footer';
 import QuickLinkFooter from './Component/QuickLinkFooter';
-import SelectActionCard from './Component/SelectActionCard';
+// import SelectActionCard from './Component/SelectActionCard';
+import AboutUs from './Pages/AboutUs';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
 
 import ContactUs from './Pages/ContactUs'
 
@@ -17,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/intportal" element={<Home />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/aboutus" element={<AboutUs/>}/> */}
         {/* Add other routes as needed */}
       </Routes>
